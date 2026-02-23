91d7bcc593eb7eb9fc321825924499e40b1aa3fe
arunkassadigigrain <arun.k@digigrain.in>
2026-02-23 15:43:19 +0530

    Update tripRoutes.js

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -10,7 +10,7 @@ router.post("/createTrip", verifyToken, TripController.createTrip);
 router.post("/verifyOtp", TripController.verifyTripOTP);
 router.patch("/updateTrip/:id", verifyToken, TripController.updateTrip);
 router.delete("/deleteTrip/:id", verifyToken, TripController.deletetrip);
-router.get("/getTrip/:id", verifyToken, TripController.gettrip);
+router.get("/getTrip/:id", TripController.gettrip);
 router.get("/getAllTrips", verifyToken, TripController.getAllTrips);
 router.get("/getTripByTime/:range", verifyToken, TripController.getTrips);
 router.get("/getTripsByDateRange/:range", verifyToken, TripController.countTripsStatus);
